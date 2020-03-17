003c5c477f5796e9244c8c3fbbb5a7a347235bc7
Tiago Felipe <tiagofsv95@gmail.com>
2020-03-16 22:56:35 -0300

    Done. Redux Saga include.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
 
@@ -8,16 +10,20 @@ import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
 
+// import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
+      {/** </Provider><Router history={history}> */}
       <BrowserRouter>
         <GlobalStyles />
         <Header />
         <Routes />
+        <ToastContainer autoClose={5000} />
       </BrowserRouter>
+      {/** </Router> */}
     </Provider>
   );
 }
